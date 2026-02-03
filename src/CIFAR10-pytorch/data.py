92276eafa9bbe7abd26a08ce8c4bca411c8436ae
George Perdrizet <george@perdrizet.org>
2026-02-03 15:06:31 +0000

    Moved pytorch functions to  in anticipation of publication as PyPI package

--- src/CIFAR10-pytorch/data.py
+++ src/CIFAR10-pytorch/data.py
@@ -1,11 +1,14 @@
 '''Data download function for CIFAR-10 dataset. Use to pre-download data
 during devcontainer creation'''
 
+from pathlib import Path
 from torchvision import datasets
 
-def download_cifar10_data(data_dir: str='data/'):
+def download_cifar10_data(data_dir: str='data/pytorch/CIFAR10'):
     '''Download CIFAR-10 dataset using torchvision.datasets.'''
 
+    data_dir = Path(data_dir)
+    data_dir.mkdir(parents=True, exist_ok=True)
 
     _ = datasets.CIFAR10(
         root=data_dir,
